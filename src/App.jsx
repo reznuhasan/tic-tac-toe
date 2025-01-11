f44905ca17a3fee28f2da9fe32e27b595f9376bc
Rizone Hasan <reznuhasan31@gmail.com>
2025-01-11 18:45:01 +0600

    initial game making done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,32 @@ import { useState } from 'react'
 import './App.css'
 import Square from './components/Square/Square'
 
+const checkText=(newValues,text)=>{
+  const arr=newValues.filter(value=>value.text===text);
+  const arrId=arr.map((value)=>value.id)
+  return arrId;
+}
+const checkWinner=(newArray)=>{
+  if([0,1,2].every(id=>newArray.includes(id))){
+    return true;
+  }else if([0,3,6].every(id=>newArray.includes(id))){
+    return true;
+  }else if([0,4,8].every(id=>newArray.includes(id))){
+    return true;
+  }else if([3,4,5].every(id=>newArray.includes(id))){
+    return true;
+  }else if([6,7,8].every(id=>newArray.includes(id))){
+    return true;
+  }else if([1,4,7].every(id=>newArray.includes(id))){
+    return true;
+  }else if([2,5,8].every(id=>newArray.includes(id))){
+    return true;
+  }else if([2,4,6].every(id=>newArray.includes(id))){
+    return true;
+  }else{
+    return false;
+  }
+}
 function App() {
   const [values,setValues]=useState([
     {id:0,text:""},
@@ -25,8 +51,17 @@ function App() {
       setCheckValue('X')
 
     }
-    
-    setValues(newValues)
+    const xArray=checkText(newValues,"X");
+    const zeroArray=checkText(newValues,'0');
+    const xWinner=checkWinner(xArray);
+    const zeroWinner=checkWinner(zeroArray);
+    if(xWinner){
+      alert("Second Player Winner")
+    }else if(zeroWinner){
+      alert("First Player Winner")
+    }else {
+      setValues(newValues)
+    }
     
   }
   return (
